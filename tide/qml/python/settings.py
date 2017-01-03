87d2b2322da2ef80676ea913295c5a46c6356708
eekkelund <eetu.kahelin@metropolia.fi>
2017-01-03 23:43:42 +0200

    Bug fix: If no config option in conf file option & value will be added when trying to get value

--- tide/qml/python/settings.py
+++ tide/qml/python/settings.py
@@ -6,6 +6,25 @@ import configparser
 import os
 
 dataPath=None
+editorDefaults={'darktheme': 'True',
+'fontsize': '35',
+'fonttype': "Sail Sans Pro Light",
+'linenums': 'True',
+'autosave': 'True',
+'indentsize': '4',
+'textcolor':"#cfbfad",
+'qmlcolor':"#ff8bff",
+'keycolor':"#808bed",
+'propertiescolor':"#ff5555",
+'jscolor':"#8888ff",
+'strcolor':"#ffcd8b",
+'commentcolor':"#cd8b00",
+'bgcolor':"#1e1e27",
+'trace':'False',
+'hint':'0',
+'plugins':'False',
+'wrapmode': 3
+}
 
 def setDataPath(path):
     confPath = path+"/harbour-tide.conf"
@@ -13,25 +32,7 @@ def setDataPath(path):
         os.makedirs(path)
     if not os.path.exists(confPath):
         config = configparser.RawConfigParser()
-        config['editor']={'darktheme': 'True',
-                          'fontsize': '35',
-                          'fonttype': "Sail Sans Pro Light",
-                          'linenums': 'True',
-                          'autosave': 'True',
-                          'indentsize': '4',
-                          'textcolor':"#cfbfad",
-                          'qmlcolor':"#ff8bff",
-                          'keycolor':"#808bed",
-                          'propertiescolor':"#ff5555",
-                          'jscolor':"#8888ff",
-                          'strcolor':"#ffcd8b",
-                          'commentcolor':"#cd8b00",
-                          'bgcolor':"#1e1e27",
-                          'trace':'False',
-                          'hint':'0',
-                          'plugins':'False',
-                          'wrapmode': 3
-                          }
+        config['editor']=editorDefaults
         with open(confPath, 'a+') as cfg:
             config.write(cfg)
     global dataPath
@@ -49,8 +50,10 @@ def set(key, value):
             config.write(configfile)
 
 def get(key):
-    config = configparser.RawConfigParser()
+    config = configparser.RawConfigParser(defaults = editorDefaults)
     if(dataPath!=None):
         config.read(dataPath)
+        if not config.has_option('editor',key):
+            config.set('editor', key, default)
         result = config['editor'][key]
         return result
