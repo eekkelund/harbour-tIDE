4eb15b9d51dfd768813782cd30f171b0c6eee642
Åke Engelbrektson <eson57@users.noreply.github.com>
2017-01-20 10:06:27 +0100

    Create harbour-tide-sv.ts

---
+++ tide/translations/harbour-tide-sv.ts
@@ -0,0 +1,625 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name></name>
+    <message id="filemanager-la-file_details">
+        <source>%1, %2</source>
+        <extracomment>Shows size and modification date, e.g. &quot;15.5MB, 02/03/2016&quot;</extracomment>
+        <translation>%1, %2</translation>
+    </message>
+    <message id="filemanager-la-no_files">
+        <source>No files</source>
+        <translation>Inga filer</translation>
+    </message>
+</context>
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>GitHub</source>
+        <translation>GitHub</translation>
+    </message>
+    <message>
+        <source>Report an issue</source>
+        <translation>Rapportera ett problem</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <source>Version</source>
+        <translation>Version</translation>
+    </message>
+    <message>
+        <source>Developer</source>
+        <translation>Utvecklare</translation>
+    </message>
+    <message>
+        <source>Credits</source>
+        <translation>Tack</translation>
+    </message>
+    <message>
+        <source>Features</source>
+        <translation>Funktioner</translation>
+    </message>
+    <message>
+        <source>Building a RPM(experimental)</source>
+        <translation>Kompilera en RPM (experimentell)</translation>
+    </message>
+    <message>
+        <source>Normal text editor features including:</source>
+        <translation>Vanliga textredigeringsfunktioner:</translation>
+    </message>
+    <message>
+        <source>Font settings</source>
+        <translation>Teckensnittsinställning</translation>
+    </message>
+    <message>
+        <source>Indentation</source>
+        <translation>Indrag</translation>
+    </message>
+    <message>
+        <source>Redo/Undo</source>
+        <translation>Ångra/Upprepa</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Sök</translation>
+    </message>
+    <message>
+        <source></source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Instructions</source>
+        <translation>Instruktioner</translation>
+    </message>
+    <message>
+        <source>License: GPLv3</source>
+        <translation>Licens: GPLv3</translation>
+    </message>
+    <message>
+        <source>Dedicated to</source>
+        <translation>Dedikerad till</translation>
+    </message>
+    <message>
+        <source>transportable IDE for SailfishOS devices.</source>
+        <translation>transportabel IDE för SailfishOS-enheter.</translation>
+    </message>
+    <message>
+        <source>Basic IDE features:</source>
+        <translation>Grundläggande IDE-funktioner:</translation>
+    </message>
+    <message>
+        <source>Autocomplete (Installs new keyboard)</source>
+        <translation>Autokomplettering (Installerar ny tangentbordslayout)</translation>
+    </message>
+    <message>
+        <source>Syntax highlighting (QML, JS &amp; Python)</source>
+        <translation>Syntax-markering (QML, JS &amp; Python)</translation>
+    </message>
+    <message>
+        <source>Project template creation for SailfishOS</source>
+        <translation>Projektmallar för SailfishOS</translation>
+    </message>
+    <message>
+        <source>Running your application</source>
+        <translation>Körning av ditt program</translation>
+    </message>
+    <message>
+        <source>Application output &amp; debug log</source>
+        <translation>Programutdata &amp; felsökningslogg</translation>
+    </message>
+    <message>
+        <source>Predictive text (QML, JS &amp; Python)</source>
+        <translation>Textigenkänning (QML, JS &amp; Python)</translation>
+    </message>
+    <message>
+        <source>Line numbers (experimental)</source>
+        <translation>Radnummer (experimentell)</translation>
+    </message>
+    <message>
+        <source>Autosave</source>
+        <translation>Spara automatiskt</translation>
+    </message>
+    <message>
+        <source>Themes</source>
+        <translation>Teman</translation>
+    </message>
+    <message>
+        <source>New keyboard is activated from: Settings - Text input - Keyboards - Develop</source>
+        <translation>Ny tangentbordslayout aktiveras från: Inställningar &gt; Textinmatning &gt; Utveckla</translation>
+    </message>
+    <message>
+        <source>Built RPM&apos;s are located at:</source>
+        <translation>Kompilerade RPM:er lagras i:</translation>
+    </message>
+    <message>
+        <source>Base of the keyboard: Jolla&apos;s</source>
+        <translation>Tangentbordsbas: Jollas</translation>
+    </message>
+    <message>
+        <source>Some highlighting rules and indentation:</source>
+        <translation>Några markeringsregler och indrag:</translation>
+    </message>
+    <message>
+        <source>SettingsPage slider:</source>
+        <translation>Inställningssidans skjutreglage:</translation>
+    </message>
+    <message>
+        <source>Icon:</source>
+        <translation>Ikon_</translation>
+    </message>
+    <message>
+        <source>and SaberAltria&apos;s</source>
+        <translation>och SaberAltria&apos;s</translation>
+    </message>
+    <message>
+        <source>Launch from terminal (harbour-tide /path/to/file.txt)</source>
+        <translation>Starta från terminal (harbour-tide /sökväg/till/fil.txt)</translation>
+    </message>
+    <message>
+        <source>Projects are located at:</source>
+        <translation>Projekt lagras i:</translation>
+    </message>
+    <message>
+        <source>Contributors</source>
+        <translation>Medarbetare</translation>
+    </message>
+    <message>
+        <source>Project deleting, wrap mode setting for the editor, Python bug fixes</source>
+        <translation>Projektborttagning, radbrytningsinställning för redigeraren, fixar för Python-fel</translation>
+    </message>
+    <message>
+        <source>Thank you! :)</source>
+        <translation>Tack! :)</translation>
+    </message>
+    <message>
+        <source>Installing built RPM</source>
+        <translation>Installerar kompilerad RPM</translation>
+    </message>
+    <message>
+        <source>Change files on the fly</source>
+        <translation>Ändra filer i farten</translation>
+    </message>
+    <message>
+        <source>Split view. And possibility to move separator</source>
+        <translation>Delad vy, och möjlighet att flytta separatorn</translation>
+    </message>
+    <message>
+        <source>Root mode features:</source>
+        <translation>Root-lägesfunktioner:</translation>
+    </message>
+    <message>
+        <source>Possibility to run applications in /usr/share</source>
+        <translation>Möjlighet att köra appar i /usr/share</translation>
+    </message>
+    <message>
+        <source>Keyboard:</source>
+        <translation>Tangenbord:</translation>
+    </message>
+    <message>
+        <source>Predictive text depending on what file opened</source>
+        <translation>Förutsägande text beroende på vilken fil som öppnas</translation>
+    </message>
+    <message>
+        <source>Tabulator button on Sym view</source>
+        <translation>Tabulatorknapp i Sym-vy</translation>
+    </message>
+    <message>
+        <source>Arrow keys</source>
+        <translation>Piltangenter</translation>
+    </message>
+    <message>
+        <source>Basic hardware support including common shortcuts</source>
+        <translation>Grundläggande hårdvarustöd inklusive vanliga genvägar</translation>
+    </message>
+    <message>
+        <source>Edit UI, app or system files. You name it!</source>
+        <translation>Redigera UI-, app- eller systemfiler. Ditt val!</translation>
+    </message>
+</context>
+<context>
+    <name>AddFileDialog</name>
+    <message>
+        <source>Add</source>
+        <translation>Lägg till</translation>
+    </message>
+    <message>
+        <source>Name of the file</source>
+        <translation>Filnamn</translation>
+    </message>
+    <message>
+        <source>Filetype</source>
+        <translation>Filtyp</translation>
+    </message>
+</context>
+<context>
+    <name>AppOutput</name>
+    <message>
+        <source>Application output</source>
+        <translation>Programutdata</translation>
+    </message>
+    <message>
+        <source>Copy</source>
+        <translation>Kopiera</translation>
+    </message>
+</context>
+<context>
+    <name>BuildOutput</name>
+    <message>
+        <source>Build output</source>
+        <translation>Kompileringsutdata</translation>
+    </message>
+    <message>
+        <source>Copy</source>
+        <translation>Kopiera</translation>
+    </message>
+    <message>
+        <source>Install the RPM</source>
+        <translation>Installera RPM:en</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Application Cover</source>
+        <translation>Programminiatyr</translation>
+    </message>
+</context>
+<context>
+    <name>CreateProject</name>
+    <message>
+        <source>Name of the project</source>
+        <translation>Projektnamn</translation>
+    </message>
+    <message>
+        <source>Create project</source>
+        <translation>Skapa projekt</translation>
+    </message>
+    <message>
+        <source>Create new project</source>
+        <translation>Skapa nytt projekt</translation>
+    </message>
+</context>
+<context>
+    <name>CreatorHome</name>
+    <message>
+        <source>Create new project</source>
+        <translation>Skapa nytt projekt</translation>
+    </message>
+    <message>
+        <source>No projects yet</source>
+        <translation>Det finns inga projekt</translation>
+    </message>
+    <message>
+        <source>Projects</source>
+        <translation>Projekt</translation>
+    </message>
+    <message>
+        <source>Open an existing project</source>
+        <translation>Öppna befintligt projekt</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Ta bort</translation>
+    </message>
+    <message>
+        <source>/usr/share/</source>
+        <translation>/usr/share/</translation>
+    </message>
+    <message>
+        <source>SD Card</source>
+        <translation>SD-kort</translation>
+    </message>
+    <message>
+        <source>Open existing project</source>
+        <translation>Öppna befintligt projekt</translation>
+    </message>
+    <message>
+        <source>Go to SD Card</source>
+        <translation>Gå till SD-kortet</translation>
+    </message>
+    <message>
+        <source>Go to own projects</source>
+        <translation>Gå till egna projekt</translation>
+    </message>
+    <message>
+        <source>Deleting project</source>
+        <translation>Tar bort projektet</translation>
+    </message>
+</context>
+<context>
+    <name>EditorPage</name>
+    <message>
+        <source>Open file</source>
+        <translation>Öppna fil</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Sök</translation>
+    </message>
+    <message>
+        <source>Tap to show top bar</source>
+        <translation>Tryck för att visa toppfält</translation>
+    </message>
+</context>
+<context>
+    <name>FileManagerPage</name>
+    <message>
+        <source>Add file</source>
+        <translation>Lägg till fil</translation>
+    </message>
+    <message>
+        <source>Go to home</source>
+        <translation>Gå till home</translation>
+    </message>
+    <message>
+        <source>Go to root</source>
+        <translation>Gå till root</translation>
+    </message>
+    <message>
+        <source>Go SD Card</source>
+        <translation>Gå till SD-kort</translation>
+    </message>
+    <message>
+        <source>Go /usr/share</source>
+        <translation>Gå till /usr/share</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Show Second page</source>
+        <translation>Visa andra sidan</translation>
+    </message>
+    <message>
+        <source>Python example</source>
+        <translation>Python-exempel</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Projects</source>
+        <translation>Projekt</translation>
+    </message>
+    <message>
+        <source>Edit single file</source>
+        <translation>Redigera enskild fil</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>About &amp; Help</source>
+        <translation>Om &amp; Hjälp</translation>
+    </message>
+    <message>
+        <source>transportable IDE</source>
+        <translation>transportabel IDE</translation>
+    </message>
+</context>
+<context>
+    <name>ProjectHome</name>
+    <message>
+        <source>Run the app</source>
+        <translation>Kör appen</translation>
+    </message>
+    <message>
+        <source>Add file</source>
+        <translation>Lägg till fil</translation>
+    </message>
+    <message>
+        <source>Select file to open</source>
+        <translation>Välj fil att öppna</translation>
+    </message>
+    <message>
+        <source>Build the app</source>
+        <translation>Kompilera appen</translation>
+    </message>
+    <message>
+        <source>Delete project</source>
+        <translation>Ta bort projekt</translation>
+    </message>
+    <message>
+        <source>Deleting project</source>
+        <translation>Tar bort projekt</translation>
+    </message>
+</context>
+<context>
+    <name>RestoreDialog</name>
+    <message>
+        <source>Restore autosaved version?</source>
+        <translation>Vill du återställa automatiskt sparad version?</translation>
+    </message>
+</context>
+<context>
+    <name>SecondPage</name>
+    <message>
+        <source>Page 2</source>
+        <translation>Sida 2</translation>
+    </message>
+    <message>
+        <source>Index</source>
+        <translation>Index</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>Font</source>
+        <translation>Teckensnitt</translation>
+    </message>
+    <message>
+        <source>Font size:</source>
+        <translation>Teckenstorlek:</translation>
+    </message>
+    <message>
+        <source>Font:</source>
+        <translation>Teckensnitt:</translation>
+    </message>
+    <message>
+        <source>Automatic</source>
+        <translation>Automatiskt</translation>
+    </message>
+    <message>
+        <source>Auto saving</source>
+        <translation>Spara automatiskt</translation>
+    </message>
+    <message>
+        <source>Indent size</source>
+        <translation>Indragsstorlek</translation>
+    </message>
+    <message>
+        <source>ExtraSmall</source>
+        <translation>Extra liten</translation>
+    </message>
+    <message>
+        <source>Small</source>
+        <translation>Liten</translation>
+    </message>
+    <message>
+        <source>Medium</source>
+        <translation>Medium</translation>
+    </message>
+    <message>
+        <source>Large</source>
+        <translation>Stor</translation>
+    </message>
+    <message>
+        <source>Sail Sans Pro Light</source>
+        <translation>Sail Sans Pro Light</translation>
+    </message>
+    <message>
+        <source>Open Sans</source>
+        <translation>Open Sans</translation>
+    </message>
+    <message>
+        <source>Helvetica</source>
+        <translation>Helvetica</translation>
+    </message>
+    <message>
+        <source>Droid Sans Mono</source>
+        <translation>Droid Sans Mono</translation>
+    </message>
+    <message>
+        <source>Comic Sans</source>
+        <translation>Comic Sans</translation>
+    </message>
+    <message>
+        <source>Ubuntu</source>
+        <translation>Ubuntu</translation>
+    </message>
+    <message>
+        <source>DejaVu Sans Mono</source>
+        <translation>DejaVu Sans Mono</translation>
+    </message>
+    <message>
+        <source>Off</source>
+        <translation>Av</translation>
+    </message>
+    <message>
+        <source>Appearance &amp; Theme</source>
+        <translation>Utseende &amp; tema</translation>
+    </message>
+    <message>
+        <source>Color of:</source>
+        <translation>Färg på:</translation>
+    </message>
+    <message>
+        <source>Line numbers</source>
+        <translation>Radnummer</translation>
+    </message>
+    <message>
+        <source>Dark Theme</source>
+        <translation>Mörkt tema</translation>
+    </message>
+    <message>
+        <source>Debugging</source>
+        <translation>Felsökning</translation>
+    </message>
+    <message>
+        <source>QML TRACE</source>
+        <translation>QML TRACE</translation>
+    </message>
+    <message>
+        <source>Text</source>
+        <translation>Text</translation>
+    </message>
+    <message>
+        <source>QML Highlight</source>
+        <translation>QML highlight</translation>
+    </message>
+    <message>
+        <source>Keywords Highlight</source>
+        <translation>Nyckelord highlight</translation>
+    </message>
+    <message>
+        <source>Properties Highlight</source>
+        <translation>Egenskaper highlight</translation>
+    </message>
+    <message>
+        <source>Javascript Highlight</source>
+        <translation>Javascript highlight</translation>
+    </message>
+    <message>
+        <source>String Highlight</source>
+        <translation>Sträng highlight</translation>
+    </message>
+    <message>
+        <source>Comment Highlight</source>
+        <translation>Kommentar highlight</translation>
+    </message>
+    <message>
+        <source>Background</source>
+        <translation>Bakgrund</translation>
+    </message>
+    <message>
+        <source>Ambience Theme</source>
+        <translation>Ambience-tema</translation>
+    </message>
+    <message>
+        <source>DEBUG PLUGINS</source>
+        <translation>FELSÖK INSTICKSMODULER</translation>
+    </message>
+    <message>
+        <source>Wrap mode:</source>
+        <translation>Radbrytningsläge:</translation>
+    </message>
+    <message>
+        <source>No wrap</source>
+        <translation>Ingen radbrytning</translation>
+    </message>
+    <message>
+        <source>Word wrap</source>
+        <translation>Radbryt vid hela ord</translation>
+    </message>
+    <message>
+        <source>Wrap anywhere</source>
+        <translation>Radbryt var som helst</translation>
+    </message>
+    <message>
+        <source>Try to word wrap, otherwise anywhere</source>
+        <translation>Försök radbryta vid hela ord</translation>
+    </message>
+    <message>
+        <source>Tab size</source>
+        <translation>Tab-storlek</translation>
+    </message>
+    <message>
+        <source>Real tab</source>
+        <translation>Äkta tab</translation>
+    </message>
+    <message>
+        <source>spaces</source>
+        <translation>Blanksteg</translation>
+    </message>
+</context>
+</TS>
